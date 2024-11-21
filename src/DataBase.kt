e50bf0ec93a9c39079bd89bc5a1a3ecef1ecafa3
Slayder12 <gnutenko.s.r@gmail.com>
2024-11-21 23:16:52 +1000

    create_data

---
+++ src/DataBase.kt
@@ -0,0 +1,13 @@
+class DataBase {
+    val smartPhoneList = listOf(
+        Smartphone("Samsung", "S20", 10000.0),
+        Smartphone("Samsung", "S21", 12000.0),
+        Smartphone("Xiaomi", "Redmi Note 10", 7000.0),
+        Smartphone("Xiaomi", "Redmi Note 11", 8000.0),
+        Smartphone("Xiaomi", "Redmi Note 12", 9000.0),
+        Smartphone("Xiaomi", "Redmi Note 13", 10000.0),
+        Smartphone("Xiaomi", "Redmi Note 14", 11000.0),
+        Smartphone("Xiaomi", "Redmi Note 15", 12000.0),
+        Smartphone("Xiaomi", "Redmi Note 16", 13000.0),
+    )
+}
